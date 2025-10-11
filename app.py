65fe85d7580de18c62e56a6a8f6c4807a41f1a2e
bhasith786 <bhasithshaik786@gmail.com>
2025-10-11 18:08:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,3 @@ def submissions():
 
     return render_template_string(html)
 
-# ---- START SERVER ----
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
